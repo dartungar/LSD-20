b0e16abc0622c7711ffb3095b535eae07a6dbcfc
danila nikolayev <d.nikolayev@treolan.ru>
2021-05-18 12:18:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LSD_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fakeData = new Data();
+            fakeData.GenerateFakeUsers(20);
+            Console.WriteLine(fakeData.Users[5].Username);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSD_20
+{
+    public enum Sex{
+      Male,
+      Famale,
+      Other // just kidding
+    }
+
+    public class User {
+      public int Id {get;set;}
+      public string Username {get;set;}
+      public Datetime Birthday {get;set;}
+      public Sex Sex {get;set;}
+    }
+}
